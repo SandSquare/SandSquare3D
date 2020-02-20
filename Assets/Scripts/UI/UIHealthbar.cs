c80e644c365ee8cecbf0946f016ab781e258f6e1
Kirage <tommi.makelainen@tuni.fi>
2020-02-20 17:25:42 +0200

    HealthbarUI init

---
+++ Assets/Scripts/UI/UIHealthbar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHealthbar : MonoBehaviour
+{
+    public Camera cameraToLookAt;
+    void Update()
+    {
+       
+        Vector3 v = cameraToLookAt.transform.position - transform.position;
+        //Vector3 v = Vector3.forward;
+        v.x = v.z = 0.0f;
+        transform.LookAt(cameraToLookAt.transform.position - v);
+        //transform.LookAt(v);
+        transform.Rotate(0, 180, 0);
+    }
+}
